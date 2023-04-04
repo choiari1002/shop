a0d09aca8064b2bb464455dc7cd9c762bc7d8947
Ari Choi <choiari1002@gmail.com>
2023-04-03 22:45:22 -0700

    more items from server

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useState } from "react";
 import Data from './Data.js';
 import Detail from './Pages/Detail.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import axios from 'axios';
 
 function App() {
 
-  let [shoes] = useState(Data);
+  let [shoes, setShoes] = useState(Data);
+  let [loading, setLoading] = useState(false);
   let navigate =  useNavigate();
 
   return (
@@ -37,7 +39,16 @@ function App() {
               }
             </div>
           </div>
-          <button>버튼</button>
+          {
+              loading == true ? <div className="alert alert-warning">Loading...</div> : null
+          }
+          <button onClick={()=>{
+            setLoading(true)
+            axios.get('https://codingapple1.github.io/shop/data2.json')
+            .then((response)=>{ console.log(response.data); let copy = [...shoes, ...response.data]; setShoes(copy); setLoading(false);})
+            .catch(()=>{ console.log('error'); setLoading(false); })
+
+            }}>More</button>
         </>}/>
         <Route path="/detail/:id" element={<><Detail shoes={shoes}></Detail></>}/>
         <Route path="*" element={<>404 PAGE</>} />
@@ -51,7 +62,7 @@ function Card(props) {
     <div className="col-md-4">
       <img src={"https://codingapple1.github.io/shop/shoes"+ (props.i+1) + ".jpg"} width="80%"/>
       <h4>{ props.shoes[props.i].title }</h4>
-      <p>${ props.shoes[props.i].price }</p>
+      <p>{ props.shoes[props.i].price }</p>
     </div>
   );
 }
