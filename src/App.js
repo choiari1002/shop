18c56d6fd84503dc6228f46a6d767b84bea68908
Ari Choi <choiari1002@gmail.com>
2023-04-03 21:47:52 -0700

    add draft detail page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./App.css";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useState } from "react";
-import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
+import Data from './Data.js';
+import Detail from './Pages/Detail.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes] = useState(Data);
+  let navigate =  useNavigate();
 
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Solemate</Navbar.Brand>
+          <Navbar.Brand onClick={()=>{ navigate('/') }}>Solemate</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Detail</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            {/* <Link to='/'>홈</Link>
-            <Link to='/detail'>상세페이지</Link> */}
+            <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/cart') }}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -38,21 +38,19 @@ function App() {
             </div>
           </div>
         </>}/>
-        <Route path="/detail" element={<>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-              </div>
-              <div className="col-md-6">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
-                <button className="btn btn-danger">주문하기</button>
-              </div>
-            </div>
-          </div>
+        <Route path="/detail/:id" element={<>
+          <Detail shoes={shoes}></Detail>
         </>}/>
+
+        <Route path="/about" element={<About></About>}>
+          <Route path="member" element={<>멤버임</>}/>
+          <Route path="location" element={<>위치정보임</>}/>
+        </Route>
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<>첫 주문시 양배추즙 서비스</>}/>
+          <Route path="two" element={<>생일기념 쿠폰받기</>}/>
+        </Route>
+        <Route path="*" element={<>404 PAGE</>} />
       </Routes>
     </div>
   );
@@ -66,6 +64,24 @@ function Card(props) {
       <p>${ props.shoes[props.i].price }</p>
     </div>
   );
+}
+
+function About() {
+  return(
+    <div>
+      <h4>About</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 export default App;
