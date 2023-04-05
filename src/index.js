d3b8e380ae41ff38cd313a5cadf95b438e819c01
Ari Choi <choiari1002@gmail.com>
2023-04-05 15:56:16 -0700

    Install react-redux

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-    <App />
+     <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
